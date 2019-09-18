67a3b4efcff8237adfeafcc10a52d848c5a41cf1
Nicola De Lazzari <nicola.delazzari@gmail.com>
2019-09-18 21:03:27 +0200

    fix profile props

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,32 +5,36 @@ import MealList from "./MealList";
 
 class Profile extends React.Component {
   render() {
-    return (
-      <Fragment>
-        <h1>Hi {this.props.profile.name.value}</h1>
-        <Link to={{ pathname: "/", name: this.props.profile.name.value }}>
-          edit profile
-        </Link>
-        {/* <ul>
-          {Object.keys(this.props.profile).map(
-            key => console.log(this.props.profile)
-            // if (this.props.profile[key].visibleOnProfile) {
-            //   <li key={key}>
-            //     {key}: {this.props.profile[key]}
-            //   </li>;
-            // }
-          )}
-        </ul> */}
+    if (this.props.profile.name) {
+      return (
+        <Fragment>
+          <h1>Hi {this.props.profile.name.value}</h1>
+          <Link to={{ pathname: "/", name: this.props.profile.name.value }}>
+            edit profile
+          </Link>
+          <ul>
+            {Object.keys(this.props.profile).map(key => {
+              if (this.props.profile[key].visibleOnProfile) {
+                return (
+                  <li key={key}>
+                    {key}: {this.props.profile[key].value}
+                  </li>
+                );
+              }
+            })}
+          </ul>
 
-        <AddMealForm addToList={this.props.addToList} />
-        <MealList meal={this.props.state.food} />
-        {this.props.total ? (
-          <p>
-            Total: <strong>{this.props.total}kcal</strong>
-          </p>
-        ) : null}
-      </Fragment>
-    );
+          <AddMealForm addToList={this.props.addToList} />
+          <MealList meal={this.props.state.food} />
+          {this.props.total ? (
+            <p>
+              Total: <strong>{this.props.total}kcal</strong>
+            </p>
+          ) : null}
+        </Fragment>
+      );
+    }
+    return <p>loading...</p>;
   }
 }
 
