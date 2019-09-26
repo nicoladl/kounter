22a7dcf8c0c0210004431a1a3e780da41709bc5c
Nicola <nicola.delazzari@octoplus.ch>
2019-09-26 10:39:34 +0200

    Updates

---
+++ precache-manifest.6eb6aac837363d1cded8a5a452f0d30d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab1c5ec9838d7891263095bb7977dd4",
+    "url": "/nicoladl/kounter/index.html"
+  },
+  {
+    "revision": "f5af436059980715d78b",
+    "url": "/nicoladl/kounter/static/js/2.d9a43901.chunk.js"
+  },
+  {
+    "revision": "43102fd115e9caa7be03",
+    "url": "/nicoladl/kounter/static/js/main.6712e86e.chunk.js"
+  },
+  {
+    "revision": "393ce6f77f1dc1e8743e",
+    "url": "/nicoladl/kounter/static/js/runtime~main.a1be4bfc.js"
+  }
+]);
