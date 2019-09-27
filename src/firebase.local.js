2631ab234e17f6e7e05d55b626816043d23d4149
Nicola <nicola.delazzari@octoplus.ch>
2019-09-27 16:20:17 +0200

    fix firebase conf

--- src/firebase.local.js
+++ src/firebase.local.js
@@ -3,9 +3,9 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-apiKey",
-  authDomain: "your-authDomain",
-  databaseURL: "your-databaseURL"
+  apiKey: "AIzaSyBA8Gwx7omYnoSCZujw-35mQPy0PANM51M",
+  authDomain: "kounter-92315.firebaseapp.com",
+  databaseURL: "https://kounter-92315.firebaseio.com"
 };
 
 // Initialize Firebase
