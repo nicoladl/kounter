82a1c12aa3ab5b348f38338eb64e1ac685d99cc3
Nicola <nicola.delazzari@octoplus.ch>
2019-09-27 16:20:54 +0200

    delete config

--- src/firebase.local.js
+++ src/firebase.local.js
@@ -3,9 +3,7 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBA8Gwx7omYnoSCZujw-35mQPy0PANM51M",
-  authDomain: "kounter-92315.firebaseapp.com",
-  databaseURL: "https://kounter-92315.firebaseio.com"
+  // your config
 };
 
 // Initialize Firebase
