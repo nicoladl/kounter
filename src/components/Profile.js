c9726bafbb718d7e374e5512a2b3d85ae347d4d2
Nicola De Lazzari <nicoladelazzari@Nicolas-MacBook-Pro.local>
2019-11-06 10:07:10 +0100

    fix no return error for map function

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,40 +11,41 @@ class Profile extends React.Component {
     const profile = props.profile;
 
     // if component is not ready show loading
-    if (profile.name) {
-      return (
-        <Fragment>
-          <h1>Hi {profile.name.value}</h1>
-          <Link to={{ pathname: "/", profile: profile }}>edit profile</Link>
-
-          <ul>
-            {Object.keys(profile).map(key => {
-              // filter if visibleOnProfile is true
-              if (profile[key].visibleOnProfile) {
-                return (
-                  // show on profile
-                  <li key={key}>
-                    {key}: {profile[key].value} {key === "weight" ? "kg" : null}
-                    {key === "height" ? "cm" : null}
-                  </li>
-                );
-              }
-            })}
-          </ul>
-
-          <AddMealForm addToList={props.addToList} state={props.state} />
-          <MealList meal={props.state.food} />
-
-          {props.state.total ? (
-            <ShowTotals
-              total={props.state.total}
-              deltaKcal={props.state.total - profile.basalMetabolism.value}
-            />
-          ) : null}
-        </Fragment>
-      );
+    if (!profile.name) {
+      return <p>loading...</p>;
     }
-    return <p>loading...</p>;
+
+    return (
+      <Fragment>
+        <h1>Hi {profile.name.value}</h1>
+        <Link to={{ pathname: "/", profile: profile }}>edit profile</Link>
+
+        <ul>
+          {Object.keys(profile).forEach(key => {
+            // filter if visibleOnProfile is true
+            if (profile[key].visibleOnProfile) {
+              return (
+                // show on profile
+                <li key={key}>
+                  {key}: {profile[key].value} {key === "weight" ? "kg" : null}
+                  {key === "height" ? "cm" : null}
+                </li>
+              );
+            }
+          })}
+        </ul>
+
+        <AddMealForm addToList={props.addToList} state={props.state} />
+        <MealList meal={props.state.food} />
+
+        {props.state.total ? (
+          <ShowTotals
+            total={props.state.total}
+            deltaKcal={props.state.total - profile.basalMetabolism.value}
+          />
+        ) : null}
+      </Fragment>
+    );
   }
 }
 
