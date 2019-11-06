40a0f2bc7a9402a98ef7ff9be0ee617916d5e8a6
Nicola De Lazzari <nicoladelazzari@Nicolas-MacBook-Pro.local>
2019-11-06 10:07:42 +0100

    import only database package

--- src/firebase.local.js
+++ src/firebase.local.js
@@ -1,5 +1,6 @@
 import Rebase from "re-base";
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
